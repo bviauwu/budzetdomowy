6a05520f5115b552af943aac377aa20c075e8265
Basia <barbarafraczkiewicz3@gmail.com>
2026-04-21 17:19:03 +0200

    2 funkcje, jedna tworzy element z wybieraniem czlonka a druga dodaje czlonka do tego elementu

--- script.js
+++ script.js
@@ -5,3 +5,28 @@ function save() {
     localStorage.setItem("budget", JSON.stringify(transactions));
     localStorage.setItem("members", JSON.stringify(members));
 }
+
+function renderMembersSelect() {
+    const select = document.getElementById("member");
+    select.innerHTML = "";
+
+    members.forEach(m => {
+        const option = document.createElement("option");
+        option.value = m;
+        option.textContent = m;
+        select.appendChild(option);
+    });
+}
+
+function addMember() {
+    const name = document.getElementById("memberName").value.trim();
+    if (!name) return;
+
+    if (!members.includes(name)) {
+        members.push(name);
+        save();
+        renderMembersSelect();
+    }
+
+    document.getElementById("memberName").value = "";
+}
